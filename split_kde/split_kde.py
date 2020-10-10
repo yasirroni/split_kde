6e00c856ed6f6b0c22003745c4edb46577c0e646
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2020-10-10 20:17:18 +0700

    bug fix split_kde.py

--- split_kde/split_kde.py
+++ split_kde/split_kde.py
@@ -2,20 +2,57 @@ import numpy as np
 from sklearn.neighbors import KernelDensity
 from scipy.signal import argrelextrema
 
-def split_kde(x, model=None, start_end=None):
-    if model == None:
-        model = KernelDensity(kernel='gaussian', bandwidth=(max(x)-min(x))/100)
-    model.fit(x)
+def split_kde(x, model=None, start_end=None, n_groups=None, lower_bound=None, upper_bound=None, max_iter=100):
+    '''
+    x: data
+    model: sklearn.neighbors.KernelDensity
+    start_end: (optional) linspace start and end point
+    n_groups: (optional) specify number of groups after splitted
+    lower_bound: (optional) if n_groups, lower bound for binary search
+    upper_bound: (optional) if n_groups, upper bound for binary search
+    max_iter: maximum number of iteration
+    '''
     if start_end == None:
-        start = max(x)
-        end = min(x)
+        start = min(x)
+        end = max(x)
     else:
         start, end = (start_end)
+    
+    if model == None:
+        model = KernelDensity(kernel='gaussian', bandwidth=(end-start)/100)
+    
     model.s_ = np.linspace(start,end)
+    model.fit(x)
     model.e_ = model.score_samples(model.s_.reshape(-1,1))
-    model.split_points_ = get_split_points_(model.e_, model.s_)
+    mi = argrelextrema(model.e_, np.less)[0]
+    model.mi_ = mi
+    if n_groups:
+        # binary_search
+        lower_bound = 0
+        iter = 0
+        while len(mi) != (n_groups-1):
+            print(model.bandwidth, model.s_[mi])
+            if len(mi) > (n_groups-1):
+                # increase kernel
+                lower_bound = model.bandwidth
+                if upper_bound:
+                    model.bandwidth = lower_bound + (upper_bound-lower_bound) / 2
+                else:
+                    model.bandwidth = lower_bound * 2
+            else:
+                # decrease kernel
+                upper_bound = model.bandwidth
+                model.bandwidth = lower_bound + (upper_bound-lower_bound) / 2
+            model.fit(x)
+            model.e_ = model.score_samples(model.s_.reshape(-1,1))
+            mi = argrelextrema(model.e_, np.less)[0]
+            iter += 1
+            if iter == max_iter:
+                raise Exception('No convergence. Try remove start_end or reduce the difference, or increase max_iter')
+    
+    model.split_points_ = model.s_[mi]    
     if model.split_points_.size == 0:
-        model.splitted_ = [x]
+        model.splitted_ = [np.array(x).flatten()]
         model.labels_ = [0]*len(x)
         return model
     else:
@@ -24,7 +61,7 @@ def split_kde(x, model=None, start_end=None):
     return model
 
 def get_split_points_(e, s):
-    mi = argrelextrema(e, np.less)[0]
+    mi = get_mi(e)  
     return s[mi]
 
 def get_splitted_(x, split_points_):
